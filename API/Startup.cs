a07d84803e2380a8bb2a370ee5fb20e663eee935
mau duong <mauduong.dev@gmail.com>
2021-04-11 11:03:19 +0700

    Fix urls

--- API/Startup.cs
+++ API/Startup.cs
@@ -44,6 +44,7 @@ namespace API
         app.UseSwaggerUI(c =>
         {
           c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+          c.RoutePrefix = string.Empty;
         });
       }
 
