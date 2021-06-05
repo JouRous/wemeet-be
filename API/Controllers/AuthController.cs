f358a00d1b1f62153338c58c19dbee131b3c79ca
duong mau <dpm11234@gmail.com>
2021-06-05 16:20:09 +0700

    Fix save file

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -18,156 +18,156 @@ using API.Services;
 
 namespace API.Controllers
 {
-    public class AuthController : BaseApiController
+  public class AuthController : BaseApiController
+  {
+    private readonly ITokenService _tokenService;
+    private readonly IMapper _mapper;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+    private readonly RoleManager<AppRole> _roleManager;
+    private readonly IWebHostEnvironment _hostEnvironment;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IEmailService _emailService;
+
+    public AuthController(
+      UserManager<AppUser> userManager,
+      SignInManager<AppUser> signInManager,
+      RoleManager<AppRole> roleManager,
+      ITokenService tokenService,
+      IUnitOfWork unitOfWork,
+      IMapper mapper,
+      IWebHostEnvironment hostEnvironment,
+      IEmailService emailService
+    )
     {
-        private readonly ITokenService _tokenService;
-        private readonly IMapper _mapper;
-        private readonly UserManager<AppUser> _userManager;
-        private readonly SignInManager<AppUser> _signInManager;
-        private readonly RoleManager<AppRole> _roleManager;
-        private readonly IWebHostEnvironment _hostEnvironment;
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IEmailService _emailService;
-
-        public AuthController(
-          UserManager<AppUser> userManager,
-          SignInManager<AppUser> signInManager,
-          RoleManager<AppRole> roleManager,
-          ITokenService tokenService,
-          IUnitOfWork unitOfWork,
-          IMapper mapper,
-          IWebHostEnvironment hostEnvironment,
-          IEmailService emailService
-        )
-        {
-            _tokenService = tokenService;
-            _mapper = mapper;
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _roleManager = roleManager;
-            _hostEnvironment = hostEnvironment;
-            _unitOfWork = unitOfWork;
-            _emailService = emailService;
-        }
-
-        private async Task<bool> CheckUserExist(string email)
-        {
-            return await _userManager.Users.AnyAsync(user => user.Email == email);
-        }
-
-        [HttpPost("register")]
-        public async Task<ActionResult<Response<AuthModel>>> Register([FromForm] RegisterModel registerModel)
-        {
-            if (await CheckUserExist(registerModel.Email))
-            {
-                return BadRequest("User already taken");
-            }
-
-            var transaction = await DbContext.Database.BeginTransactionAsync();
-            var user = _mapper.Map<AppUser>(registerModel);
-
-            user.UserName = user.Email;
-            user.Avatar = await SaveImage(registerModel.AvatarFile);
-            user.AppUserTeams = new List<AppUserTeam>();
-
-            var createStatus = await _userManager.CreateAsync(user, registerModel.Password);
-
-            if (!createStatus.Succeeded)
-            {
-                await transaction.RollbackAsync();
-                return BadRequest(createStatus.Errors);
-            }
-
-            var addRoleStatus = await _userManager.AddToRoleAsync(user, "Staff");
-
-            if (!addRoleStatus.Succeeded)
-            {
-                await transaction.RollbackAsync();
-                return BadRequest(addRoleStatus.Errors);
-            }
-
-            if (registerModel.TeamId != 0)
-            {
-                var appUserTeam = new AppUserTeam
-                {
-                    AppUserId = user.Id,
-                    TeamId = registerModel.TeamId
-                };
-
-                user.AppUserTeams.Add(appUserTeam);
-                var addTeamResult = await _userManager.UpdateAsync(user);
-
-                if (!addTeamResult.Succeeded)
-                {
-                    await transaction.RollbackAsync();
-                    return BadRequest();
-                }
-            }
-
-            transaction.Commit();
-
-            await _emailService.sendMailAsync(user.Email, "Dang ky thanh cong.", "Mat khau la 123123");
-
-            var auth = new AuthModel
-            {
-                token = await _tokenService.CreateToken(user),
-                User = _mapper.Map<UserDTO>(user),
-                Role = await _userManager.GetRolesAsync(user)
-            };
-
-            return new Response<AuthModel>
-            {
-                status = 200,
-                success = true,
-                Data = auth
-            };
-
-        }
-
-        [HttpPost("login")]
-        public async Task<ActionResult<Response<AuthModel>>> Login(LoginModel loginModel)
-        {
-            var user = await _userManager.Users.SingleOrDefaultAsync(x => x.Email == loginModel.Email.ToLower());
-
-            if (user == null)
-            {
-                return Unauthorized("Invalid User");
-            }
-
-            var result = await _signInManager.CheckPasswordSignInAsync(user, loginModel.Password, false);
-
-            if (!result.Succeeded)
-            {
-                return Unauthorized();
-            }
-
-            var authDTO = new AuthModel
-            {
-                User = _mapper.Map<UserDTO>(user),
-                token = await _tokenService.CreateToken(user),
-                Role = await _userManager.GetRolesAsync(user)
-            };
-
-            return new Response<AuthModel>
-            {
-                Data = authDTO,
-                success = true,
-                status = 200
-            };
-        }
-
-        private async Task<string> SaveImage(IFormFile imageFile)
-        {
-            string imageName = DateTime.Now.ToString("yyyymmssfff") + "_" + Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
-
-            var uploadPath = Path.Combine(_hostEnvironment.ContentRootPath, "Uploads", "Avatars", imageName);
-
-            using (var fileStream = new FileStream(uploadPath, FileMode.Create))
-            {
-                await imageFile.CopyToAsync(fileStream);
-            }
-
-            return imageName;
-        }
+      _tokenService = tokenService;
+      _mapper = mapper;
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _roleManager = roleManager;
+      _hostEnvironment = hostEnvironment;
+      _unitOfWork = unitOfWork;
+      _emailService = emailService;
     }
+
+    private async Task<bool> CheckUserExist(string email)
+    {
+      return await _userManager.Users.AnyAsync(user => user.Email == email);
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<Response<AuthModel>>> Register([FromForm] RegisterModel registerModel)
+    {
+      if (await CheckUserExist(registerModel.Email))
+      {
+        return BadRequest("User already taken");
+      }
+
+      var transaction = await DbContext.Database.BeginTransactionAsync();
+      var user = _mapper.Map<AppUser>(registerModel);
+
+      user.UserName = user.Email;
+      user.Avatar = registerModel.AvatarFile != null ? await SaveImage(registerModel.AvatarFile) : null;
+      user.AppUserTeams = new List<AppUserTeam>();
+
+      var createStatus = await _userManager.CreateAsync(user, registerModel.Password);
+
+      if (!createStatus.Succeeded)
+      {
+        await transaction.RollbackAsync();
+        return BadRequest(createStatus.Errors);
+      }
+
+      var addRoleStatus = await _userManager.AddToRoleAsync(user, "Staff");
+
+      if (!addRoleStatus.Succeeded)
+      {
+        await transaction.RollbackAsync();
+        return BadRequest(addRoleStatus.Errors);
+      }
+
+      if (registerModel.TeamId != 0)
+      {
+        var appUserTeam = new AppUserTeam
+        {
+          AppUserId = user.Id,
+          TeamId = registerModel.TeamId
+        };
+
+        user.AppUserTeams.Add(appUserTeam);
+        var addTeamResult = await _userManager.UpdateAsync(user);
+
+        if (!addTeamResult.Succeeded)
+        {
+          await transaction.RollbackAsync();
+          return BadRequest();
+        }
+      }
+
+      transaction.Commit();
+
+      await _emailService.sendMailAsync(user.Email, "Dang ky thanh cong.", "Mat khau la 123123");
+
+      var auth = new AuthModel
+      {
+        token = await _tokenService.CreateToken(user),
+        User = _mapper.Map<UserDTO>(user),
+        Role = await _userManager.GetRolesAsync(user)
+      };
+
+      return new Response<AuthModel>
+      {
+        status = 200,
+        success = true,
+        Data = auth
+      };
+
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<Response<AuthModel>>> Login(LoginModel loginModel)
+    {
+      var user = await _userManager.Users.SingleOrDefaultAsync(x => x.Email == loginModel.Email.ToLower());
+
+      if (user == null)
+      {
+        return Unauthorized("Invalid User");
+      }
+
+      var result = await _signInManager.CheckPasswordSignInAsync(user, loginModel.Password, false);
+
+      if (!result.Succeeded)
+      {
+        return Unauthorized();
+      }
+
+      var authDTO = new AuthModel
+      {
+        User = _mapper.Map<UserDTO>(user),
+        token = await _tokenService.CreateToken(user),
+        Role = await _userManager.GetRolesAsync(user)
+      };
+
+      return new Response<AuthModel>
+      {
+        Data = authDTO,
+        success = true,
+        status = 200
+      };
+    }
+
+    private async Task<string> SaveImage(IFormFile imageFile)
+    {
+      string imageName = DateTime.Now.ToString("yyyymmssfff") + "_" + Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+
+      var uploadPath = Path.Combine(_hostEnvironment.ContentRootPath, "Uploads", "Avatars", imageName);
+
+      using (var fileStream = new FileStream(uploadPath, FileMode.Create))
+      {
+        await imageFile.CopyToAsync(fileStream);
+      }
+
+      return imageName;
+    }
+  }
 }
