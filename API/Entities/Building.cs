9603e8a04fd1692644fc93e920b8b136b270e36c
William To <18520248@gm.uit.edu.vn>
2021-06-10 19:51:48 +0700

    add entity

---
+++ API/Entities/Building.cs
@@ -0,0 +1,9 @@
+namespace API.Entities
+{
+	public class Building : Bases
+	{
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public int NumberRooms { get; set; }
+	}
+}
