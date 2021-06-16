674ac610fd5904d0111ac8270c14450b1d11eaae
mau duong <mauduong.dev@gmail.com>
2021-06-16 20:51:34 +0700

    Change db to mysql

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using API.DTO;
 using API.Entities;
 using API.Interfaces;
+using API.Models;
+using API.Services;
 using API.Types;
 using API.Utils;
 using AutoMapper;
@@ -13,81 +15,166 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-	[Authorize]
-	public class UsersController : BaseApiController
-	{
-		private readonly UserManager<AppUser> _userManager;
-		private readonly IMapper _mapper;
-		private readonly IUnitOfWork _unitOfWork;
+  [Authorize]
+  public class UsersController : BaseApiController
+  {
+    private readonly UserManager<AppUser> _userManager;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IEmailService _emailService;
+    private readonly ITokenService _tokenService;
 
-		public UsersController(IMapper mapper, UserManager<AppUser> userManager, IUnitOfWork unitOfWork)
-		{
-			_userManager = userManager;
-			_mapper = mapper;
-			_unitOfWork = unitOfWork;
-		}
+    public UsersController(
+      IMapper mapper,
+      UserManager<AppUser> userManager,
+      IUnitOfWork unitOfWork,
+      ITokenService tokenService,
+      IEmailService emailService
+      )
+    {
+      _userManager = userManager;
+      _mapper = mapper;
+      _unitOfWork = unitOfWork;
+      _emailService = emailService;
+      _tokenService = tokenService;
+    }
 
+    private async Task<bool> CheckUserExist(string email)
+    {
+      return await _userManager.Users.AnyAsync(user => user.Email == email);
+    }
 
-		[HttpGet("{username}")]
-		public async Task<ActionResult<UserDTO>> GetUser(string username)
-		{
-			return await _unitOfWork.USerRepository.GetUserAsync(username);
-		}
+    [HttpPost("create-user")]
+    public async Task<ActionResult<Response<AuthModel>>> CreateUser([FromBody] RegisterModel registerModel)
+    {
+      if (await CheckUserExist(registerModel.Email))
+      {
+        return BadRequest("User already taken");
+      }
 
-		[HttpGet]
-		public async Task<ActionResult<Response<IEnumerable<UserDTO>>>> GetUsers(
-		  [FromQuery] PaginationParams paginationParams)
-		{
-			var token = await HttpContext.GetTokenAsync("access_token");
-			var handler = new JwtSecurityTokenHandler();
-			var roles = handler.ReadJwtToken(token)
-					   .Claims.Where(c => c.Type.Equals("role")).Select(c => c.Value).ToList();
+      var transaction = await DbContext.Database.BeginTransactionAsync();
+      var user = _mapper.Map<AppUser>(registerModel);
 
-			var checkRole = roles.Any(role => role.Equals("Admin"));
+      user.UserName = user.Email;
+      user.AppUserTeams = new List<AppUserTeam>();
 
-			if (!checkRole)
-			{
-				return Unauthorized("Admin only! Permission denied");
-			}
+      var randomPassword = Utils.Utils.RandomString(9);
 
-			var res = await _unitOfWork.USerRepository.GetUsersAsync(paginationParams);
+      var createStatus = await _userManager.CreateAsync(user, randomPassword);
 
-			var response = new ResponseBuilder<IEnumerable<UserDTO>>()
-					   .AddData(res.Items)
-					   .AddPagination(new PaginationDTO
-					   {
-						   CurrentPage = res.CurrentPage,
-						   PageSize = res.PageSize,
-						   TotalItems = res.TotalItems
-					   })
-					   .Build();
+      if (!createStatus.Succeeded)
+      {
+        await transaction.RollbackAsync();
+        return BadRequest(createStatus.Errors);
+      }
 
-			return response;
-		}
+      var addRoleStatus = await _userManager.AddToRoleAsync(user, "Staff");
 
-		[HttpGet("me")]
-		public async Task<ActionResult> GetProfile()
-		{
-			var token = await HttpContext.GetTokenAsync("access_token");
-			var handler = new JwtSecurityTokenHandler();
+      if (!addRoleStatus.Succeeded)
+      {
+        await transaction.RollbackAsync();
+        return BadRequest(addRoleStatus.Errors);
+      }
 
-			var email = handler.ReadJwtToken(token)
-					   .Claims.Where(c => c.Type.Equals("email")).Select(c => c.Value).SingleOrDefault();
-			var roles = handler.ReadJwtToken(token)
-					   .Claims.Where(c => c.Type.Equals("role")).Select(c => c.Value).ToList();
+      if (!String.IsNullOrEmpty(registerModel.TeamId))
+      {
+        var appUserTeam = new AppUserTeam
+        {
+          AppUserId = user.Id,
+          TeamId = registerModel.TeamId
+        };
 
-			var User = await _unitOfWork.USerRepository.GetUserAsync(email);
-			var profile = new
-			{
-				User = User,
-				Roles = roles
-			};
+        user.AppUserTeams.Add(appUserTeam);
+        var addTeamResult = await _userManager.UpdateAsync(user);
 
-			return Ok(profile);
-		}
+        if (!addTeamResult.Succeeded)
+        {
+          await transaction.RollbackAsync();
+          return BadRequest();
+        }
+      }
 
-	}
+      transaction.Commit();
+
+      await _emailService.sendMailAsync(user.Email, "Dang ky thanh cong.", $"Mat khau la {randomPassword}");
+
+      var auth = new AuthModel
+      {
+        token = await _tokenService.CreateToken(user),
+        User = _mapper.Map<UserDTO>(user),
+        Role = await _userManager.GetRolesAsync(user)
+      };
+
+      return new Response<AuthModel>
+      {
+        status = 200,
+        success = true,
+        Data = auth
+      };
+
+    }
+
+    [HttpGet("{username}")]
+    public async Task<ActionResult<UserDTO>> GetUser(string username)
+    {
+      return await _unitOfWork.USerRepository.GetUserAsync(username);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<Response<IEnumerable<UserDTO>>>> GetUsers(
+      [FromQuery] PaginationParams paginationParams)
+    {
+      var token = await HttpContext.GetTokenAsync("access_token");
+      var handler = new JwtSecurityTokenHandler();
+      var roles = handler.ReadJwtToken(token)
+             .Claims.Where(c => c.Type.Equals("role")).Select(c => c.Value).ToList();
+
+      var checkRole = roles.Any(role => role.Equals("Admin"));
+
+      if (!checkRole)
+      {
+        return Unauthorized("Admin only! Permission denied");
+      }
+
+      var res = await _unitOfWork.USerRepository.GetUsersAsync(paginationParams);
+
+      var response = new ResponseBuilder<IEnumerable<UserDTO>>()
+             .AddData(res.Items)
+             .AddPagination(new PaginationDTO
+             {
+               CurrentPage = res.CurrentPage,
+               PageSize = res.PageSize,
+               TotalItems = res.TotalItems
+             })
+             .Build();
+
+      return response;
+    }
+
+    [HttpGet("me")]
+    public async Task<ActionResult> GetProfile()
+    {
+      var token = await HttpContext.GetTokenAsync("access_token");
+      var handler = new JwtSecurityTokenHandler();
+
+      var email = handler.ReadJwtToken(token)
+             .Claims.Where(c => c.Type.Equals("email")).Select(c => c.Value).SingleOrDefault();
+      var roles = handler.ReadJwtToken(token)
+             .Claims.Where(c => c.Type.Equals("role")).Select(c => c.Value).ToList();
+
+      var User = await _unitOfWork.USerRepository.GetUserAsync(email);
+      var profile = new
+      {
+        User = User,
+        Roles = roles
+      };
+
+      return Ok(profile);
+    }
+
+  }
 }
