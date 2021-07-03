c0dd4b05b366fa1278a1ef29a512680342f71908
mau duong <mauduong.dev@gmail.com>
2021-07-03 12:35:49 +0700

    Edit cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -16,74 +16,74 @@ using API.Configs;
 
 namespace API
 {
-  public class Startup
-  {
-    private readonly IConfiguration _config;
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      _config = configuration;
-    }
-
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddAppServices(_config);
-      services.AddIdentityServices(_config);
-      services.AddControllers();
-      services.AddCors(options =>
-                  {
-                    options.AddPolicy("CorsPolicy", builder => builder
-                      .WithOrigins(_config["FE_URL"])
-                        .AllowAnyMethod()
-                        .AllowAnyHeader()
-                        .AllowCredentials());
-                  });
-      services.AddSignalR();
-      services.AddMvcCore()
-        .AddJsonOptions(opt => opt.JsonSerializerOptions.PropertyNamingPolicy = new SnakeCaseNamingPolicy());
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      });
-    }
-
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        // app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration configuration)
         {
-          c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-          c.RoutePrefix = string.Empty;
-        });
-      }
+            _config = configuration;
+        }
 
-      app.UseMiddleware<ExceptionHandler>();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAppServices(_config);
+            services.AddIdentityServices(_config);
+            services.AddControllers();
+            services.AddCors(options =>
+                        {
+                            options.AddPolicy("CorsPolicy", builder => builder
+                        .WithOrigins(_config["FE_URL"])
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowCredentials());
+                        });
+            services.AddSignalR();
+            services.AddMvcCore()
+              .AddJsonOptions(opt => opt.JsonSerializerOptions.PropertyNamingPolicy = new SnakeCaseNamingPolicy());
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+        }
 
-      app.UseHttpsRedirection();
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                // app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                    c.RoutePrefix = string.Empty;
+                });
+            }
 
-      app.UseStaticFiles(new StaticFileOptions()
-      {
-        FileProvider = new PhysicalFileProvider(
-          Path.Combine(Directory.GetCurrentDirectory(), @"Uploads/Avatars")
-        ),
-        RequestPath = new PathString("/uploads/avatar")
-      });
+            app.UseMiddleware<ExceptionHandler>();
 
-      app.UseRouting();
+            app.UseHttpsRedirection();
 
-      app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("*"));
-      app.UseCors("CorsPolicy");
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                Path.Combine(Directory.GetCurrentDirectory(), @"Uploads/Avatars")
+              ),
+                RequestPath = new PathString("/uploads/avatar")
+            });
 
-      app.UseAuthentication();
-      app.UseAuthorization();
+            app.UseRouting();
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-        endpoints.MapHub<NotificationService>("/notification");
-      });
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<NotificationService>("/notification");
+            });
+        }
     }
-  }
 }
