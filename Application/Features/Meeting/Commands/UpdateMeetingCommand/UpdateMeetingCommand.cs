c5f4c60d73caf0114f571eaa5728400f82b901ae
mau duong <mauduong.dev@gmail.com>
2021-07-09 10:06:06 +0700

    Fix Update Meeting

--- Application/Features/Meeting/Commands/UpdateMeetingCommand/UpdateMeetingCommand.cs
+++ Application/Features/Meeting/Commands/UpdateMeetingCommand/UpdateMeetingCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace Application.Features.Commands
 {
@@ -9,18 +10,19 @@ namespace Application.Features.Commands
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Note { get; set; }
         public DateTime? StartTime { get; set; }
         public DateTime? EndTime { get; set; }
         public Guid Room_Id { get; set; }
         public ICollection<Guid> Team_Ids { get; set; }
-        public ICollection<int> users_in_meeting { get; set; }
-        public ICollection<Guid> Tag_Ids { get; set; }
+        public ICollection<int> users_in_meeting { get; set; } = new List<int>();
+        public ICollection<Guid> Tag_Ids { get; set; } = new List<Guid>();
+        public ICollection<IFormFile> Attachments { get; set; } = new List<IFormFile>();
 
+        public UpdateMeetingCommand()
+        { }
         public UpdateMeetingCommand(Guid Id)
         {
             this.Id = Id;
         }
-
     }
 }
